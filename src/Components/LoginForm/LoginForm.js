b6bb20c80ebb2b2a18b7e066987044da5f7cf315
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-14 12:12:04 -0400

    created sections for form

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './LoginForm.css';
 
 class LoginForm extends Component {
     constructor(props) {
@@ -8,12 +9,42 @@ class LoginForm extends Component {
     render() { 
         return ( 
             <section className='LoginForm'>
-                <form className='LoginForm__form'>
+                <form className='LoginForm-form'>
                     <fieldset>
                         <legend>
-                            Login
+                            Login Page
                         </legend>
-                        
+                        <div className='sign-up-div'>
+                            <div className='div-header'>
+                                <div className='left-line'></div>
+                                <div className='center-text'>New User</div>
+                                <div className='right-line'></div>
+                            </div>
+                            <div className='sign-up-button'>
+                                <button
+                                    type='button'
+                                    className='app-button'
+                                >
+                                    Sign Up
+                                </button>
+                            </div>
+                        </div>
+                        <div className='login-div'>
+                        <div className='div-header'>
+                                <div className='left-line'></div>
+                                <div className='center-text'>Existing User</div>
+                                <div className='right-line'></div>
+                            </div>
+                        </div>
+                        <div className='submit-button-div'>
+                            <button 
+                                type='submit'
+                                className='app_button'
+                                // onSubmit=''
+                            >
+                                Login
+                            </button>
+                        </div>
 
                      
                     </fieldset>
