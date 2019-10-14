9242514afb5e07c2cd21ee80434c824b1f699f2c
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-14 10:35:07 -0400

    create base route to go to loginform

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
+import LoginForm from '../LoginForm/LoginForm';
 
 export default class App extends Component {
 
@@ -16,11 +17,27 @@ export default class App extends Component {
     // get data from the database and set state
   }
 
+  renderRoutes = () => {
+    const loginPath = [
+      '/'
+    ]
+
+    const loginRoute = <Route 
+      exact
+      path={loginPath}
+      component={LoginForm}
+      />
+
+    return {
+      loginRoute,
+    }
+  }
+
   render(){
     return (
       <BrowserRouter>
         <div className='App'>
-          
+          {this.renderRoutes().loginRoute}
         </div>
       </BrowserRouter>
     )
