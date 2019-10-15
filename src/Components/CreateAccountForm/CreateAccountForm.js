1a7d6f7ea7111ddf1a59a9b365d168a1be21521b
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-15 11:55:14 -0400

    created Form Components for creating account

---
+++ src/Components/CreateAccountForm/CreateAccountForm.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './CreateAccountForm.css';
+
+class CreateAccountForm extends Component {
+    
+    render() { 
+        return (  
+            <form className='CreateAccountForm'>
+                CreateAccountForm
+            </form>
+        );
+    }
+}
+ 
+export default CreateAccountForm;
