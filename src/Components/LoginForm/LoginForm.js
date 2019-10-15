ef0e82b6c10072531f6c2bc0c7d62ddfd86ac98a
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-15 10:26:10 -0400

    removed extra div

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -14,78 +14,76 @@ class LoginForm extends Component {
 
         return (
             <section className='LoginForm'>
-                <div className='LoginForm-child'>
-                    <header>
-                        <div className='logo'>
-                            <FontAwesomeIcon icon='shipping-fast' className='truck-icon' />
-                            <div className='app-title'>
-                                <span className='xpress'>Xpress</span>
-                                <span className='dispatcher'>Dispatcher</span>
+                <header>
+                    <div className='logo'>
+                        <FontAwesomeIcon icon='shipping-fast' className='truck-icon' />
+                        <div className='app-title'>
+                            <span className='xpress'>Xpress</span>
+                            <span className='dispatcher'>Dispatcher</span>
+                        </div>
+                    </div>
+                </header>
+                <form className='LoginForm-form'>
+                    <fieldset>
+                        <legend>
+                            <h2>Login Page</h2>
+                        </legend>
+                        <div className='sign-up-div'>
+                            <div className='div-header'>
+                                <div className='left-line'></div>
+                                <div className='center-text'>New User</div>
+                                <div className='right-line'></div>
+                            </div>
+                            <div className='sign-up-button'>
+                                <Link to='/register' className='app-button'>
+                                    Create Account
+                                    </Link>
                             </div>
                         </div>
-                    </header>
-                    <form className='LoginForm-form'>
-                        <fieldset>
-                            <legend>
-                                <h2>Login Page</h2>
-                            </legend>
-                            <div className='sign-up-div'>
-                                <div className='div-header'>
-                                    <div className='left-line'></div>
-                                    <div className='center-text'>New User</div>
-                                    <div className='right-line'></div>
-                                </div>
-                                <div className='sign-up-button'>
-                                    <Link to='/register' className='app-button'>
-                                        Create Account
-                                    </Link>
-                                </div>
+                        <div className='login-div'>
+                            <div className='div-header'>
+                                <div className='left-line'></div>
+                                <div className='center-text'>Existing User</div>
+                                <div className='right-line'></div>
                             </div>
-                            <div className='login-div'>
-                                <div className='div-header'>
-                                    <div className='left-line'></div>
-                                    <div className='center-text'>Existing User</div>
-                                    <div className='right-line'></div>
-                                </div>
-                                <div className='login-inputs'>
-                                    <label htmlFor='user-name'>
-                                        <span className='input-label'>
-                                            Username:
+                            <div className='login-inputs'>
+                                <label htmlFor='user-name'>
+                                    <span className='input-label'>
+                                        Username:
                                     </span>
-                                        <input
-                                            type='text'
-                                            required
-                                            id='user-name'
-                                            name='user-name'
-                                            className='user-name'
-                                        />
-                                    </label>
-                                    <span className='error-message'>Invalid Username</span>
-                                    <label htmlFor='password'>
-                                        <span className='input-label'>
-                                            Password:
+                                    <input
+                                        type='text'
+                                        required
+                                        id='user-name'
+                                        name='user-name'
+                                        className='user-name'
+                                    />
+                                </label>
+                                <span className='error-message'>Invalid Username</span>
+                                <label htmlFor='password'>
+                                    <span className='input-label'>
+                                        Password:
                                     </span>
-                                        <input
-                                            type='password'
-                                            required
+                                    <input
+                                        type='password'
+                                        required
 
-                                        />
-                                    </label>
-                                    <span className='error-message'>Password did not match</span>
-                                </div>
+                                    />
+                                </label>
+                                <span className='error-message'>Password did not match</span>
                             </div>
-                            <div className='submit-button-div'>
-                                <span className='forgot-password'>Forgot Password?</span>
-                                <button
-                                    type='submit'
-                                    className='app-button'
-                                >
-                                    Login
+                        </div>
+                        <div className='submit-button-div'>
+                            <span className='forgot-password'>Forgot Password?</span>
+                            <button
+                                type='submit'
+                                className='app-button'
+                            >
+                                Login
                             </button>
-                            </div>
-                        </fieldset>
-                    </form>
-                </div>
+                        </div>
+                    </fieldset>
+                </form>
             </section>
         );
     }
