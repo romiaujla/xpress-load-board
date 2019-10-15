4faaf8e04783d3ac20202e951ba477022e04e1da
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-15 11:31:45 -0400

    Refactored LoginPage and LoginForm to only have form in it

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import './LoginForm.css';
 import { Link } from 'react-router-dom';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
-
 
 class LoginForm extends Component {
     constructor(props) {
@@ -13,78 +10,69 @@ class LoginForm extends Component {
     render() {
 
         return (
-            <section className='LoginForm'>
-                <header>
-                    <div className='logo'>
-                        <FontAwesomeIcon icon='shipping-fast' className='truck-icon' />
-                        <div className='app-title'>
-                            <span className='xpress'>Xpress</span>
-                            <span className='dispatcher'>Dispatcher</span>
+
+
+            <form className='LoginForm-form'>
+                <fieldset>
+                    <legend>
+                        <h2>Login Page</h2>
+                    </legend>
+                    <div className='sign-up-div'>
+                        <div className='div-header'>
+                            <div className='left-line'></div>
+                            <div className='center-text'>New User</div>
+                            <div className='right-line'></div>
+                        </div>
+                        <div className='sign-up-button'>
+                            <Link to='/register' className='app-button'>
+                                Create Account
+                            </Link>
                         </div>
                     </div>
-                </header>
-                <form className='LoginForm-form'>
-                    <fieldset>
-                        <legend>
-                            <h2>Login Page</h2>
-                        </legend>
-                        <div className='sign-up-div'>
-                            <div className='div-header'>
-                                <div className='left-line'></div>
-                                <div className='center-text'>New User</div>
-                                <div className='right-line'></div>
-                            </div>
-                            <div className='sign-up-button'>
-                                <Link to='/register' className='app-button'>
-                                    Create Account
-                                    </Link>
-                            </div>
+                    <div className='login-div'>
+                        <div className='div-header'>
+                            <div className='left-line'></div>
+                            <div className='center-text'>Existing User</div>
+                            <div className='right-line'></div>
                         </div>
-                        <div className='login-div'>
-                            <div className='div-header'>
-                                <div className='left-line'></div>
-                                <div className='center-text'>Existing User</div>
-                                <div className='right-line'></div>
-                            </div>
-                            <div className='login-inputs'>
-                                <label htmlFor='user-name'>
-                                    <span className='input-label'>
-                                        Username:
+                        <div className='login-inputs'>
+                            <label htmlFor='user-name'>
+                                <span className='input-label'>
+                                    Username:
                                     </span>
-                                    <input
-                                        type='text'
-                                        required
-                                        id='user-name'
-                                        name='user-name'
-                                        className='user-name'
-                                    />
-                                </label>
-                                <span className='error-message'>Invalid Username</span>
-                                <label htmlFor='password'>
-                                    <span className='input-label'>
-                                        Password:
+                                <input
+                                    type='text'
+                                    required
+                                    id='user-name'
+                                    name='user-name'
+                                    className='user-name'
+                                />
+                            </label>
+                            <span className='error-message'>Invalid Username</span>
+                            <label htmlFor='password'>
+                                <span className='input-label'>
+                                    Password:
                                     </span>
-                                    <input
-                                        type='password'
-                                        required
+                                <input
+                                    type='password'
+                                    required
 
-                                    />
-                                </label>
-                                <span className='error-message'>Password did not match</span>
-                            </div>
+                                />
+                            </label>
+                            <span className='error-message'>Password did not match</span>
                         </div>
-                        <div className='submit-button-div'>
-                            <span className='forgot-password'>Forgot Password?</span>
-                            <button
-                                type='submit'
-                                className='app-button'
-                            >
-                                Login
+                    </div>
+                    <div className='submit-button-div'>
+                        <span className='forgot-password'>Forgot Password?</span>
+                        <button
+                            type='submit'
+                            className='app-button'
+                        >
+                            Login
                             </button>
-                        </div>
-                    </fieldset>
-                </form>
-            </section>
+                    </div>
+                </fieldset>
+            </form>
         );
     }
 }
---
+++ src/Routes/LoginPage/LoginPage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import LoginForm from '../../Components/LoginForm';
+
+export default class LoginPage extends Component {
+
+    render() {
+        return (
+            <section classNam='LoginPage'>
+                <LoginForm />
+            </section>
+        );
+    }
+}
